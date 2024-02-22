48254fd15e278d3df65788b37758bf204fefc683
Pau Debionne <Paudebionne@paucasesnovescifp.cat>
2024-02-22 20:58:29 +0100

    Mejora maxima

--- Buscaminas.js
+++ Buscaminas.js
@@ -1,6 +1,21 @@
 /*Pau y Jose*/
 const board = document.getElementById("tablero");
 const message = document.getElementById("mensaje");
+const easy = {
+    minas: 10,
+    tamano: 10,
+    button: document.getElementById("facil")
+};
+const medium = {
+    minas: 40,
+    tamano: 15,
+    button: document.getElementById("medio")
+};
+const hard = {
+    minas: 70,
+    tamano: 20,
+    button: document.getElementById("dificil")
+};
 let gridSize,
     mineCount,
     minesRemaining,
@@ -79,11 +94,11 @@ function revealCell(row, col) {
     }
     else if (cell.neighbors === 0)
         for (let r = row - 1; r <= row + 1; r++)
-            for (let c = col - 1; c <= col + 1; c++)
+            for (let c = col - 1; c <= col + 1; c++) {
                 revealCell(r, c);
-    updateBoard();
+            }
+    updateBoard(row, col);
     if (revealedCells === gridSize * gridSize - mineCount) endGame(true);
-
 }
 
 // Manejador de clics en una casilla
@@ -111,62 +126,57 @@ function updateMinesRemaining() {
 }
 
 // Función para actualizar el tablero después de revelar una casilla
-function updateBoard() {
-    for (let row = 0; row < gridSize; row++) {
-        for (let col = 0; col < gridSize; col++) {
-            const cell = board.children[row * gridSize + col];
-            if (gameBoard[row][col].isRevealed) {
-                if (gameBoard[row][col].isMine)
-                    cell.classList.add("icon-bomba");
-                else {
-                    cell.textContent = gameBoard[row][col].neighbors || ""; // Esto mete un string al div con la cantidad de vecinos (minas) que tiene la casilla
-                    cell.classList.add("destapado");
-                    cell.classList.add("c" + gameBoard[row][col].neighbors); // Esto le da el color al numero segun la canitdad de minas que tenga cerca
-                }
-            } else if (gameBoard[row][col].isFlagged)
-                cell.classList.add("icon-bandera");
-            else
-                cell.className = "cell";
+function updateBoard(row, col) {
+    const cell = board.children[row * gridSize + col];
+    if (gameBoard[row][col].isRevealed) {
+        if (gameBoard[row][col].isMine)
+            cell.classList.add("icon-bomba");
+        else {
+            cell.textContent = gameBoard[row][col].neighbors || ""; // Esto mete un string al div con la cantidad de vecinos (minas) que tiene la casilla
+            cell.classList.add("destapado");
+            cell.classList.add("c" + gameBoard[row][col].neighbors); // Esto le da el color al numero segun la canitdad de minas que tenga cerca
         }
-    }
+    } else if (gameBoard[row][col].isFlagged)
+        cell.classList.add("icon-bandera");
+    else
+        cell.className = "cell";
 }
 
 // Función para finalizar el juego (victoria o derrota)
-    function endGame(isWin) {
-        for (let row = 0; row < gridSize; row++)
-            for (let col = 0; col < gridSize; col++) {
-                const cell = board.children[row * gridSize + col];
-                cell.removeEventListener("click", handleClick);// Se desactivan los eventListener para no desperdiciar memoria
-                cell.removeEventListener("contextmenu", handleRightClick);
-                if (gameBoard[row][col].isMine && !gameBoard[row][col].isFlagged && isWin) cell.classList.add("icon-bandera"); // Si es una mina, no tiene bandera y has ganado, la celda se marca como bandera
-                if (gameBoard[row][col].isMine && !gameBoard[row][col].isFlagged && !isWin) gameBoard[row][col].isRevealed = true; // Si es una mina, no tiene bandera y has perdido, la celda se revela como mina
-                if (gameBoard[row][col].isFlagged && !gameBoard[row][col].isMine) cell.classList.add("banderaErronea"); // Si tiene una bandera donde no hay una mina, se marca como erronea
-                if (isWin && !cell.classList.contains("icon-bandera")) {
-                    cell.classList.remove("destapado");
-                    cell.textContent = ("");
-                    cell.classList.add("spawnAnimation");
-                }
+function endGame(isWin) {
+    for (let row = 0; row < gridSize; row++)
+        for (let col = 0; col < gridSize; col++) {
+            const cell = board.children[row * gridSize + col];
+            cell.removeEventListener("click", handleClick);// Se desactivan los eventListener para no desperdiciar memoria
+            cell.removeEventListener("contextmenu", handleRightClick);
+            if (gameBoard[row][col].isMine && !gameBoard[row][col].isFlagged && isWin) cell.classList.add("icon-bandera"); // Si es una mina, no tiene bandera y has ganado, la celda se marca como bandera
+            if (gameBoard[row][col].isMine && !gameBoard[row][col].isFlagged && !isWin) cell.classList.add("icon-bomba");// Si es una mina, no tiene bandera y has perdido, la celda se revela como mina
+            if (gameBoard[row][col].isFlagged && !gameBoard[row][col].isMine) cell.classList.add("banderaErronea"); // Si tiene una bandera donde no hay una mina, se marca como erronea
+            if (isWin && !cell.classList.contains("icon-bandera")) {
+                cell.classList.remove("destapado");
+                cell.textContent = ("");
+                cell.classList.add("spawnAnimation");
             }
-        message.textContent = isWin ? "Felicidades, has ganado!" : "Has perdido! Intentalo de nuevo.";
-        message.style.backgroundColor = isWin ? "lime" : "crimson";
-    }
-
+        }
+    message.textContent = isWin ? "Felicidades, has ganado!" : "Has perdido! Intentalo de nuevo.";
+    message.style.backgroundColor = isWin ? "lime" : "crimson";
+}
 
 function onButtonClick(str) {
     switch (str){
         case ("easy"):
-            mineCount = 10;
-            gridSize = 10;
+            mineCount = easy.minas;
+            gridSize = easy.tamano;
             start()
             break;
         case ("medium"):
-            mineCount = 40;
-            gridSize = 15;
+            mineCount = medium.minas;
+            gridSize = medium.tamano;
             start()
             break;
         case ("hard"):
-            mineCount = 80;
-            gridSize = 20;
+            mineCount = hard.minas;
+            gridSize = hard.tamano;
             start()
     }
 }
@@ -181,3 +191,7 @@ function resetBoard() {
 
     revealedCells = 0; // Reiniciar el contador de celdas reveladas
 }
+
+easy.button.textContent = "Facil Tamaño: " + easy.tamano + "  Minas: " + easy.minas;
+medium.button.textContent = "Medio Tamaño: " + medium.tamano + "  Minas: " + medium.minas;
+hard.button.textContent = "Dificil Tamaño: " + hard.tamano + "  Minas: " + hard.minas;
